da47b63af356a720ed0fd5aa23ca4cda318fdb2e
ishankjain95 <ishankjain95@gmail.com>
2018-06-22 21:30:12 +0000

    Update sampler.py

--- hlda/sampler.py
+++ hlda/sampler.py
@@ -186,7 +186,7 @@ class HierarchicalLDA(object):
 
     def estimate(self, num_samples, display_topics=50, n_words=5, with_weights=True):
 
-        print 'HierarchicalLDA sampling\n'
+        print('HierarchicalLDA sampling\n')
         for s in range(num_samples):
 
             sys.stdout.write('.')
